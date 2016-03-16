460e6a7edd72aeee3b372cbdd0ca069685c2daff
randopa <rando.pallon@gmail.com>
2016-03-16 19:38:08 +0200

    Update edit.php

--- edit.php
+++ edit.php
@@ -14,8 +14,8 @@
          <form action="save.php?id=<?php print $andmed["id"]; ?>" method="post" enctype="multipart/form-data">
                 Nimi: <input type="text" name="nimi" value="<?php print $andmed["nimi"]; ?>" required<br>
                 Telefon: <input type="text" name="tel" value="<?php print $andmed["tel"]; ?>" required<br>
-                Sugu: <input value="mmees" type="radio" name="sex" <?php print ($andmed["sex"] == "Mees" ? "checked" : ""); ?>required >Mees
-                          <input value="nnaine" type="radio" name="sex" <?php print ($andmed["sex"] == "Naine" ? "checked" : ""); ?>required >Naine<br>
+                Sugu: <input value="mmees" type="radio" name="sex" <?php print ($andmed["sex"] == "Mees" ? "checked" : ""); ?> >Mees
+                          <input value="nnaine" type="radio" name="sex" <?php print ($andmed["sex"] == "Naine" ? "checked" : ""); ?> >Naine<br>
                 Pilt:<br>
                 <img src="<?php print "./kasutaja/$id/pilt.jpg"; ?>" height="100" width="100"><br>
                 <input type="file" name="img" accept="image/*"><br>
