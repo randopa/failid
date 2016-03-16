8e6ed4bbeb2e6bd5d1cf5073eeba2e7c6961e1ed
randopa <rando.pallon@gmail.com>
2016-03-17 00:38:13 +0200

    Create save-merge.php

---
+++ save-merge.php
@@ -0,0 +1,21 @@
+<?php
+   ini_set('display_errors',1);
+   error_reporting(E_ALL);
+   require "api-merge.php";
+   $userData = array(
+           "name" => $_POST['name'],
+           "phone" => $_POST['phone'],
+           "gender" => $_POST['gender'],
+           "comment" => $_POST['comment'],
+           "displayPicture" => $_FILES['img']['tmp_name']);
+   if (isset($_GET["id"])) {
+           $userData["id"] = $_GET["id"];
+           editUser($userData);
+   }
+   else {
+           createUser($userData);
+   }
+ 
+   header("Location:http://robert.vkhk.ee/~rando.pallon/kool/III/htmljaphp/katse/all-merge.php");
+   die();
+?>
