bc411366f343c1ca4e3a12a7711a20d26575fc67
randopa <rando.pallon@gmail.com>
2016-03-16 22:49:35 +0200

    Update api-merge.php
    Mergimine Marksoniga

--- api-merge.php
+++ api-merge.php
@@ -1,97 +1,102 @@
 <?php
+     
      date_default_timezone_set('Europe/Tallinn');
      ini_set('display_errors',1);
      error_reporting(E_ALL);
-     function loo($andmed) {
-              $nimi = $andmed["nimi"];
-              $tel = $andmed["tel"];
-              $sex = $andmed["sex"];
-              $muu = $andmed["muu"];
-              $file = $andmed["pilt"];
-              $akoht = "./kasutaja";
-              $kaustad = glob($akoht . "/*", GLOB_ONLYDIR);
-              if (count($kaustad) != 0) {
-                     $hurricane = "";
-                     foreach ($kaustad as $kaust) {
-                           if (basename($kaust) > $hurricane)
+     
+     function createUser($data) {
+              $name = $data["name"];
+              $phone = $data["phone"];
+              $gender = $data["gender"];
+              $comment = $data["comment"];
+              $file = $data["displayPicture"];
+              $homeFolder = "./userID";
+              $userFolder = glob($homeFolder . "/*", GLOB_ONLYDIR);
+              if (count($userFolder) != 0) {
+                     $folderCreate = "";
+                     foreach ($userFolder as $folder) {
+                           if (basename($folder) > $folderCreate)
                            {
-                           $hurricane = basename($kaust);
+                           $folderCreate = basename($folder);
                            }
                      }
-                     $id = $hurricane + 1;
+                     $id = $folderCreate + 1;
               }
               else { 
                      $id = 0;
               }
-              $kaust = 'kasutaja/'.$id;
-              if ( !file_exists($kaust) ){
-                     $tee = umask(0);
-                     mkdir ($kaust, 0777, true);
-                     umask($tee);
+              $folder = 'userID/'.$id;
+              if ( !file_exists($folder) ){
+                     $perm = umask(0);
+                     mkdir ($folder, 0777, true);
+                     umask($perm);
               } 
             
-              $aeg = time();
-              $andmed = fopen($kaust.'/fill.json','w');
-              $fill[] = array(
-              "id" => $id,
-              "nimi" => $nimi,
-              "tel" => $tel,
-              "sex" => $sex,
-              "muu" => $muu,
-              "aeg" => $aeg);
-              move_uploaded_file($file, $kaust.'/pilt.jpg');
-              fwrite($andmed, json_encode($fill));
-              fclose($andmed);
-     }
-  function muuda($andmed) {
-              $id = $andmed["id"];
-              $nimi = $andmed["nimi"];
-              $tel = $andmed["tel"];
-              $sex = $andmed["sex"];
-              $muu = $andmed["muu"];
-              $aeg =time();
-              $file = $andmed["pilt"];     
               
-              $akoht = "./kasutaja/$id";
-              $andmed = fopen($akoht.'/fill.json','w');
-              $fill[] = array(
+              $time = time();
+              $data = fopen($folder.'/userData.json','w');
+              $userData[] = array(
+              "id" => $id,
+              "name" => $name,
+              "phone" => $phone,
+              "gender" => $gender,
+              "comment" => $comment,
+              "time" => $time);
+              move_uploaded_file($file, $folder.'/displayPicture.jpg');
+              fwrite($data, json_encode($userData));
+              fclose($data);
+              }
+  
+  function editUser($data) {
+              $id = $data["id"];
+              $name = $data["name"];
+              $phone = $data["phone"];
+              $gender = $data["gender"];
+              $comment = $data["comment"];
+              $time =time();
+              $file = $data["displayPicture"];     
+              
+              $homeFolder = "./userID/$id";
+              $data = fopen($homeFolder.'/userData.json','w');
+              $userData[] = array(
                       "id" => $id,
-                      "nimi" => $nimi,
-                      "tel" => $tel,
-                      "sex" => $sex,
-                      "muu" => $muu,
-                      "aeg" => $aeg);
-              move_uploaded_file($file, $akoht.'/pilt.jpg');
-              fwrite($andmed, json_encode($fill));
-              fclose($andmed);
-  }
-  function vaata($id) {
-    $dataPath = "./kasutaja/$id/fill.json";
-    $json = file_get_contents($dataPath);
-    $andmed = json_decode($json, true);
-      $andmed = $andmed[0];
-    $andmed["aeg"] = strftime("%d/%m/%Y %H:%M", $andmed["aeg"]);
-    return $andmed;
-  }
-  function eemalda() {
-    $reganud = [];
-    $i = 0;
+                      "name" => $name,
+                      "phone" => $phone,
+                      "gender" => $gender,
+                      "comment" => $comment,
+                      "time" => $time);
+              move_uploaded_file($file, $homeFolder.'/displayPicture.jpg');
+              fwrite($data, json_encode($userData));
+              fclose($data);
+              }
+  function viewUser($id) {
+            $dataPath = "./userID/$id/userData.json";
+            $json = file_get_contents($dataPath);
+            $data = json_decode($json, true);
+            $data = $data[0];
+            $data["time"] = strftime("%d/%m/%Y %H:%M", $data["time"]);
+            return $data;
+            
+            }
+  function userList() {
+            $users = [];
+            $i = 0;
     
-    foreach (glob('./kasutaja/*', GLOB_ONLYDIR) as $kasutaja) {
-               $id = filter_var($kasutaja, FILTER_SANITIZE_NUMBER_INT);
-               $reganud[$i] = vaata($id);
-               $i++;
-    }
-    return $reganud;
-  }
-  function delete($id) {
-    $akoht = "./kasutaja/$id";
-    if (is_dir($akoht)) {
-      $files = glob($akoht . "/*");
-      foreach ($files as $file) {
-        unlink($file);
-      }
-      rmdir($akoht);
-    }
-  }
+            foreach (glob('./userID/*', GLOB_ONLYDIR) as $userID) {
+                  $id = filter_var($userID, FILTER_SANITIZE_NUMBER_INT);
+                  $users[$i] = display($id);
+                  $i++;
+            }
+            return $users;
+            }
+  function deleteUser($id) {
+            $homeFolder = "./userID/$id";
+            if (is_dir($homeFolder)) {
+            $files = glob($homeFolder . "/*");
+            foreach ($files as $file) {
+              unlink($file);
+            }
+            rmdir($homeFolder);
+            }
+            }
 ?>
