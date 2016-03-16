394b8215f65fb156318337565389a40718969187
randopa <rando.pallon@gmail.com>
2016-03-17 00:46:06 +0200

    Create all-merge.php

---
+++ all-merge.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+         <meta charset="utf-8">
+         <title>All users</title>
+</head>
+<body>
+         <?php
+         require "api-merge.php";
+         $users = userList();
+   
+         $allUsers = '<table>';
+         foreach ($users as $oneUser) {
+            $allUsers .= '
+                 <tr>
+                    <td>' . $oneUser["id"] . '</td>
+                    <td>
+                          <img src="userID/' . $oneUser["id"] . '/displayPicture.jpg" height="50" width="50">
+</td>
+<td>' . $oneUser["name"] . '</td>
+<td>
+<a href="one-merge.php?id=' .
+$oneUser["id"] . '">View</a>
+<a href="edit.php?id=' .
+$oneUser["id"] . '">Edit</a>
+<a href="delete.php?id=' .
+$oneUser["id"] . '">Delete</a>
+</td>
+</tr>';
+}
+$allUsers .= '</table>';
+print $allUsers;
+?>
+</body>
+</html> 
