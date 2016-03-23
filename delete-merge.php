b891b5a809cfbf21bbe16a3151d7aed97d54fbed
randopa <rando.pallon@gmail.com>
2016-03-24 00:04:59 +0200

    Delete delete-merge.php

--- delete-merge.php
+++
@@ -1,6 +0,0 @@
-<?php
-      require "api-merge.php";
-      
-      deleteUser($_GET["id"]);
-      header("Location:http://robert.vkhk.ee/~rando.pallon/kool/III/htmljaphp/katse/merge/all-merge.php");
-      die(); 
